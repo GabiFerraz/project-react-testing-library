5d87318320daee4fd81da7935c31cad0af98c92c
GabiFerraz <ferrazmgabi@gmail.com>
2022-01-28 00:21:36 -0300

    requisito 2 de verdade

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -33,7 +33,8 @@ describe('Teste o componente <About.js />', () => {
   test('se a página contém a imagem de uma Pokédex', () => {
     renderWithRouter(<About />);
 
-    const img = screen.getByRole('img', { name: /Pokédex/i });
-    expect(img).toBeInTheDocument();
+    const src = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const img = screen.getByRole('img', { alt: /Pokédex/i });
+    expect(img.src).toBe(src);
   });
 });
